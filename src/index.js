0c62c26e7848122303a297f84c296d7b3f57e05c
Bill Patrianakos <bill@billpatrianakos.me>
2017-11-06 16:27:48 -0600

    Update the env var used to choose a port to run server on.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ const express = require('express'),
     res.send('Hello world');
   });
 
-var server = app.listen(process.env.port || 3000, () => {
+var server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is listening on ${server.address().port}`);
 });
