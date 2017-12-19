28290bcd9909f324ce7744662936138e202af8aa
Bill Patrianakos <bill@billpatrianakos.me>
2017-12-18 19:09:05 -0600

    Took a stab at mocking up a debts table.Migrations not run yet, nothing finalized.

--- migrations/20171211152804_create_payments_table.js
+++ migrations/20171211152804_create_payments_table.js
@@ -1,14 +1,20 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('payments', (table) => {
+    knex.schema.createTable('debts', (table) => {
       table.increments();
-      //
+      table.string('vendor');
+      table.decimal('amount_due');
+      table.decimal('balance');
+      table.date('due_date');
+      table.string('period');
       table.timestamps();
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('debts')
+  ]);
 };
